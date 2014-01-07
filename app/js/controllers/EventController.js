68c6848cc0d121357b2aa3f8318088922ab81243
Chris Jackson <crjackso@gmail.com>
2014-01-06 23:36:10 -0600

    event registration site updates

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,13 @@
+﻿eventsApp.controller('EventController', function EventController($scope) {
+    $scope.event = {
+        name: 'Birthday party',
+        date: '1/1/2013',
+        time: '10:40',
+        location: {
+            address: 'Google Headquarters',
+            city: 'Mountain View',
+            province: 'CA'
+        },
+        imageUrl: '/img/angular-logo.png'
+    };
+});
